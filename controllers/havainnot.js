edcc06f1a7f847c447b70d0e54b958e2c7c8cb55
JmmJmmJ <jy.maki@gmail.com>
2022-04-03 11:25:40 +0300

    Rakenne muutoksia

---
+++ controllers/havainnot.js
@@ -0,0 +1,74 @@
+const havainnotRouter = require("express").Router();
+const Havainto = require("../models/havainto");
+
+havainnotRouter.get("/", (req, res) => {
+  Havainto.find({}).then((result) => {
+    res.json(result);
+  });
+});
+
+havainnotRouter.get("/:id", (req, res, next) => {
+  Havainto.findById(req.params.id)
+    .then((havainto) => {
+      res.json(havainto);
+    })
+    .catch((error) => next(error));
+});
+
+havainnotRouter.delete("/:id", (request, response, next) => {
+  Havainto.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+const generateId = () => {
+  min = Math.ceil(10);
+  max = Math.floor(10000);
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+havainnotRouter.post("/", (request, response, next) => {
+  const body = request.body;
+
+  const havainto = new Havainto({
+    id: generateId(),
+    laji: body.laji,
+    paikka: body.paikka,
+    paiva: body.paiva,
+    aika: body.aika,
+    maara: body.maara,
+    kommentit: body.kommentti,
+  });
+
+  havainto
+    .save()
+    .then((savedHavainto) => {
+      response.json(savedHavainto);
+    })
+    .catch((error) => next(error));
+});
+
+havainnotRouter.put("/:id", (request, response, next) => {
+  const { laji, paikka, paiva, aika, maara, kommentit } = request.body;
+
+  Havainto.findByIdAndUpdate(
+    request.params.id,
+    {
+      laji,
+      paikka,
+      paiva,
+      aika,
+      maara,
+      kommentit,
+    },
+    { new: true, runValidators: true, context: "query" }
+  )
+    .then((updatedHavainto) => {
+      response.json(updatedHavainto);
+    })
+    .catch((error) => next(error));
+});
+
+module.exports = havainnotRouter;
--- index.js
+++ index.js
@@ -1,100 +1,10 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const morgan = require("morgan");
-const app = express();
-const Havainto = require("./models/havainto");
+const app = require("./app");
+const http = require("http");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
 
-app.use(express.json());
-app.use(cors());
-app.use(morgan("tiny"));
-app.use(express.static("build"));
+const server = http.createServer(app);
 
-app.get("/api/havainnot", (req, res) => {
-  Havainto.find({}).then((result) => {
-    res.json(result);
-  });
-});
-
-app.get("/api/havainnot/:id", (req, res, next) => {
-  Havainto.findById(req.params.id)
-    .then((havainto) => {
-      res.json(havainto);
-    })
-    .catch((error) => next(error));
-});
-
-app.delete("/api/havainnot/:id", (request, response, next) => {
-  Havainto.findByIdAndRemove(request.params.id)
-    .then((result) => {
-      response.status(204).end();
-    })
-    .catch((error) => next(error));
-});
-
-const generateId = () => {
-  min = Math.ceil(10);
-  max = Math.floor(10000);
-  return Math.floor(Math.random() * (max - min) + min);
-};
-
-app.post("/api/havainnot", (request, response, next) => {
-  const body = request.body;
-
-  const havainto = new Havainto({
-    id: generateId(),
-    laji: body.laji,
-    paikka: body.paikka,
-    paiva: body.paiva,
-    aika: body.aika,
-    maara: body.maara,
-    kommentit: body.kommentti,
-  });
-
-  havainto
-    .save()
-    .then((savedHavainto) => {
-      response.json(savedHavainto);
-    })
-    .catch((error) => next(error));
-});
-
-app.put("/api/havainnot/:id", (request, response, next) => {
-  const { laji, paikka, paiva, aika, maara, kommentit } = request.body;
-
-  Havainto.findByIdAndUpdate(
-    request.params.id,
-    {
-      laji,
-      paikka,
-      paiva,
-      aika,
-      maara,
-      kommentit,
-    },
-    { new: true, runValidators: true, context: "query" }
-  )
-    .then((updatedHavainto) => {
-      response.json(updatedHavainto);
-    })
-    .catch((error) => next(error));
-});
-
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
-
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
-  } else if (error.name === "ValidationError") {
-    return response.status(400).json({ error: error.message });
-  }
-
-  next(error);
-};
-
-app.use(errorHandler);
-
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
